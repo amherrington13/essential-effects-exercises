c8aba949930864a55a273dd9075b682524bf8ff5
Adam Herrington <adamherrington@Adams-MacBook-Air.local>
2022-04-07 10:45:36 -0500

    Exercise 2 - Ticking Clock

--- build.sbt
+++ build.sbt
@@ -6,5 +6,8 @@ lazy val root = project
     name := "my-io",
     version := "0.1.0-SNAPSHOT",
     scalaVersion := scala3Version,
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies ++= Seq(
+      "org.typelevel" %% "cats-effect" % "3.3.9",
+      "org.scalameta" %% "munit" % "0.7.29" % Test
+    )
   )
---
+++ src/main/scala/com/effects/TickingClock.scala
@@ -0,0 +1,16 @@
+package com.effects
+
+import cats.effect.*
+import scala.concurrent.duration._
+object TickingClock extends IOApp {
+
+  def run(args: List[String]): IO[ExitCode] =
+    tickingClock.as(ExitCode.Success)
+
+  val tickingClock: IO[Unit] =
+    for {
+      _ <- IO.println(System.currentTimeMillis)
+      _ <- IO.sleep(1.second)
+      _ <- tickingClock
+    } yield ()
+}
